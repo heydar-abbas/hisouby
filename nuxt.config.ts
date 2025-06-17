29ff352095e40b4308575788999cce31b31dfa34
heydar-abbas <haydarabbas40@proton.me>
2025-06-17 16:35:30 +0300

    Add favorite icon and google Kufi Noto font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,7 +14,25 @@ export default defineNuxtConfig({
   app: {
     head: {
       htmlAttrs: { dir: "rtl", lang: "ar" },
-      link: [{ rel: "icon", type: "image/x-icon", href: "/images/google.png" }],
+      link: [
+        // Faveicons
+        { rel: "icon", type: "image/png", href: "/images/favicon/favicon-96x96.png", sizes: "96x96" },
+        { rel: "icon", type: "image/svg+xml", href: "/images/favicon/favicon.svg" },
+        { rel: "shortcut icon", href: "/images/favicon/favicon.ico" },
+        { rel: "apple-touch-icon", href: "/images/favicon/apple-touch-icon.png", sizes: "180x180" },
+        { rel: "manifest", href: "/images/favicon/site.webmanifest" },
+        // Google Fonts
+        { rel: "preconnect", href: "https://fonts.googleapis.com" },
+        { rel: "preconnect", href: "https://fonts.gstatic.com", crossorigin: "anonymous" },
+        {
+          href: "https://fonts.googleapis.com/css2?family=Noto+Kufi+Arabic:wght@100..900&display=swap",
+          rel: "stylesheet",
+        },
+      ],
+      meta: [
+        { name: "theme-color", content: "#101828" },
+        { name: "apple-mobile-web-app-title", content: "Hisouby" },
+      ]
     },
   },
   runtimeConfig: {
